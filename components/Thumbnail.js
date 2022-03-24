c0baee4cd6270332e29b387e7aec01b8c4965d7d
Jack Buscombe <jackbuscombe94@gmail.com>
2022-03-24 11:48:44 +1000

    Fixed Thumbnail Linting Error

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -6,7 +6,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
 	const BASE_URL = "https://image.tmdb.org/t/p/original";
 	return (
 		<div ref={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-			<Image layout="responsive" src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} />
+			<Image layout="responsive" src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`} height={1080} width={1920} alt="" />
 			<div className="p-2">
 				<p className="truncate max-w-md">{result.overview}</p>
 				<h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_title}</h2>
@@ -17,4 +17,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
 		</div>
 	);
 });
+Thumbnail.displayName = "Thumbnail";
+
 export default Thumbnail;
